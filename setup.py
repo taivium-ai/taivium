d959a2a0de11a7f9276d5af793b64166ad5112a0
taivium-labs <taiviumlabs@gmail.com>
2026-05-26 23:01:06 +0200

    refactor: manage versioning in pyproject.toml and update setup.py accordingly

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='taivium',
-    version='0.0.1',
+    # version is now managed in pyproject.toml (PEP 621)
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     install_requires=[
@@ -22,7 +22,6 @@ setup(
     },
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.7',
