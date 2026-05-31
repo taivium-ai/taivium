47d71e875b5bc9e23c0efce635eac3e18ca2c05c
taivium-labs <taivium-labs@gmail.com>
2026-05-31 13:15:20 +0200

    remove test

--- tests/test_version.py
+++
@@ -1,6 +0,0 @@
-import taivium
-
-def test_version_exists():
-    assert hasattr(taivium, "__version__"), "taivium.__version__ should exist"
-    assert isinstance(taivium.__version__, str), "taivium.__version__ should be a string"
-    assert len(taivium.__version__) > 0, "taivium.__version__ should not be empty"
