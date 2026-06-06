8c8e25a252e1b1df07032524032f489f5c60bd4e
taivium-labs <taivium-labs@gmail.com>
2026-06-06 10:00:58 +0200

    fix test

--- tests/test_utility.py
+++ tests/test_utility.py
@@ -10,16 +10,16 @@ class TestGetGlinerModel:
 
     def test_get_gliner_model_returns_model_instance(self):
         """get_gliner_model() should return a GLiNER model instance."""
-        eng.get_gliner_model.cache_clear()
-        model = eng.get_gliner_model()
+        utility.get_gliner_model.cache_clear()
+        model = utility.get_gliner_model()
         assert model is not None
         assert hasattr(model, 'predict_entities')
 
     def test_get_gliner_model_caches_instance(self):
         """get_gliner_model() should return the same cached instance on repeated calls."""
-        eng.get_gliner_model.cache_clear()
-        model1 = eng.get_gliner_model()
-        model2 = eng.get_gliner_model()
+        utility.get_gliner_model.cache_clear()
+        model1 = utility.get_gliner_model()
+        model2 = utility.get_gliner_model()
         assert model1 is model2
 
 # --- spaCy OSError path (get_spacy_model) ---
